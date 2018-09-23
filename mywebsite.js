503fb383db811230c9385fbbdb131d87d044b499
Sidkhanna96 <skhanna1@ualberta.ca>
2018-09-23 12:48:35 -0600

    New branch created

--- mywebsite.js
+++ mywebsite.js
@@ -1,9 +1,43 @@
-var name = prompt('What is your name');
-alert(name);
+window.onbeforeunload = function () {
+		window.scrollTo(0, 0);
+	}
 
-if () {
+window.sr = ScrollReveal();
 
-}
-else () {
-	
-}
+	sr.reveal('.border',{
+		duration:3000});
+
+	sr.reveal('.card',{
+		delay: 2000,
+		duration:2000,	
+		origin:'right',
+		distance:'180px'});
+
+	sr.reveal('.card2',{
+		delay: 800,
+		duration:2000});
+
+	sr.reveal('.card3',{
+		delay: 1500,
+		duration:2000,
+		origin:'left',
+		distance:'180px'});
+
+	sr.reveal('.GitHub',{
+		delay: 3000,
+		duration:2000});
+
+	sr.reveal('.panda',{
+		delay:4000});
+
+
+
+// var name = prompt('What is your name');
+// alert(name);
+
+// if () {
+
+// }
+// else () {
+
+// }
